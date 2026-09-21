96fbbdb6dfe670d7b2ea4194f2b904c63f5e5c18
Minal Sadiq <minalsadiq310@gmail.com>
2026-09-21 17:18:03 +0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -16,11 +16,10 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-from predict import (
+from .predict import (
     MissingColumnsError,
     get_feature_importance,
     get_model_info,
-    score_batch,
 )
 
 # --------------------------------------------------------------------------
